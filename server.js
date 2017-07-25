46b516bbc518c0395cb0fde6e4521d6a2777f32c
prateek211205361719 <prateek@ceptes.com>
2017-07-25 21:28:17 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,38 @@
+
+
+
+var express = require('express');
+var app = express();
+
+var hbs = require('hbs');
+hbs.registerPartials(__dirname+'/views/partial');
+hbs.registerHelper('getDate',() => {
+    return new Date().toDateString();
+});
+app.set('view engine', hbs);
+
+
+app.use((req, res, next) => {
+        
+    //res.send('hello how are u');
+    next();
+});
+
+
+var port = process.env.port || 3000;
+app.get('/', (req, res) => {
+    //res.send('hello how are u');
+    res.render('home.hbs', {
+        title:'Home page'
+    });
+});
+app.get('/about', (req, res) => {
+    //res.send('hello how are u');
+    res.render('about.hbs', {
+        title:'About page'
+    });
+});
+app.listen(port, function(){
+    console.log('------port-----'+port);
+});
+
